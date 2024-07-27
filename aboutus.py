a00d2cc1f929c9460cc0a2ed207e49c7079e592b
KomalpreetKaur43 <123819528+KomalpreetKaur43@users.noreply.github.com>
2024-07-27 16:05:30 -0400

    Update aboutus.py

--- aboutus.py
+++ aboutus.py
@@ -30,7 +30,7 @@ def show():
     <div class="aboutus-text">
     <ul>
     <li>Introducing Basketball Video Analysis, a state-of-the-art sports video analysis tool that will transform the way you watch and comprehend basketball games. To provide you with the best in automated basketball analysis, our team of committed professionals—including data engineers, machine learning specialists, video processing experts, and UI developers—works nonstop.</li>
-    <li><strong>Our goal is to give coaches, athletes, and analysts precise, up-to-date information so they can make wise decisions, perform better, and take greater pleasure in the game. Our technology provides accurate object recognition and tactical analysis through the use of deep learning models and powerful computer vision techniques, guaranteeing that you never miss a crucial moment on the court.</strong></li>
+    <li>Our goal is to give coaches, athletes, and analysts precise, up-to-date information so they can make wise decisions, perform better, and take greater pleasure in the game. Our technology provides accurate object recognition and tactical analysis through the use of deep learning models and powerful computer vision techniques, guaranteeing that you never miss a crucial moment on the court.</li>
     </ul>
     </div>
     </div>
